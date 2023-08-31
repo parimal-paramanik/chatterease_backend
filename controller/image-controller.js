6bf2750c609c49db8f167bf6562cd552a94da09a
unknown <smartdesk2015@gmail.com>
2023-08-31 21:40:16 +0530

    backend link chnaged

--- controller/image-controller.js
+++ controller/image-controller.js
@@ -1,7 +1,7 @@
 import grid from 'gridfs-stream';
 import mongoose from 'mongoose';
 
-const url = 'http://localhost:8000';
+const url = 'https://chatter-ease.onrender.com';
 
 
 let gfs, gridfsBucket;
